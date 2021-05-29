69270fcd54421c85283587746a1df16260227851
enzo <enzo.danjour@gmail.com>
2021-05-29 16:23:12 -0300

    Login Page struture

--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginPage extends StatefulWidget {
   LoginPage({Key? key}) : super(key: key);
@@ -11,7 +12,35 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text('Divida suas contas com seus amigos',
+          style: GoogleFonts.gotu(),
+          ),
+          Column(
+            children: [
+              ListTile(
+                leading: Icon(Icons.sim_card),
+                title: Text('Faça seu login com uma das contas abaixo'),
+              ),
+              SizedBox(
+                height: 32,
+              ),
+              ElevatedButton.icon(
+                onPressed: (){}, 
+                icon: Icon(Icons.email), 
+                label: Text("Entrar com google")
+              ),
+              ElevatedButton.icon(
+                onPressed: (){}, 
+                icon: Icon(Icons.email), 
+                label: Text("Entrar com Apple")
+              ),
+            ],
+          )
+        ],
+      )
     );
   }
 }

1d3d10200601d254977e2ddc5618dd3a904c40bf
enzo <enzo.danjour@gmail.com>
2021-06-02 10:04:58 -0300

    create named routes

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:split_it/modules/login/login_page.dart';
+import 'package:split_it/modules/splash/splash_page.dart';
+
+import 'modules/error/error_page.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({Key? key}) : super(key: key);
@@ -9,7 +12,12 @@ class AppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Split-it',
-      home: LoginPage(),
+      initialRoute: "/splash",
+      routes: {
+        "/splash" : (context) => SplashPage(),
+        "/login" : (context) => LoginPage(),
+        "/error" : (context) => ErrorPage()
+      },
     );
   }
 }

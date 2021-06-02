beba30f064bae80f7c06ae54c7e1903a6d66781e
enzo <enzo.danjour@gmail.com>
2021-06-02 10:06:49 -0300

    Refactoring login icons

--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:split_it/modules/login/widgets/social_button.dart';
+import 'package:split_it/themes/app_theme.dart';
 
 class LoginPage extends StatefulWidget {
   LoginPage({Key? key}) : super(key: key);
@@ -12,35 +13,52 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Text('Divida suas contas com seus amigos',
-          style: GoogleFonts.gotu(),
-          ),
-          Column(
+        body: Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 40),
+          child: Row(
             children: [
-              ListTile(
-                leading: Icon(Icons.sim_card),
-                title: Text('Faça seu login com uma das contas abaixo'),
-              ),
-              SizedBox(
-                height: 32,
-              ),
-              ElevatedButton.icon(
-                onPressed: (){}, 
-                icon: Icon(Icons.email), 
-                label: Text("Entrar com google")
-              ),
-              ElevatedButton.icon(
-                onPressed: (){}, 
-                icon: Icon(Icons.email), 
-                label: Text("Entrar com Apple")
+              Container(
+                width: 236,
+                child: Text('Divida suas contas com seus amigos',
+                    style: AppTheme.textStyles.title),
               ),
             ],
-          )
-        ],
-      )
-    );
+          ),
+        ),
+        Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 40),
+              child: ListTile(
+                leading: Image.asset("assets/images/emoji.png"),
+                title: Text(
+                  'Faça seu login com uma das contas abaixo',
+                  style: AppTheme.textStyles.button,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 32,
+            ),
+            SocialButton(
+              imagePath: 'assets/images/google.png',
+              label: 'Entrar com Google',
+              onTap: () {},
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            SocialButton(
+              imagePath: 'assets/images/apple.png',
+              label: 'Entrar com Apple',
+              onTap: () {},
+            )
+          ],
+        )
+      ],
+    ));
   }
 }

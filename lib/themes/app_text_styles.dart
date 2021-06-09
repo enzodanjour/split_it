6a92bb39868db7b3010eab143c38d7b16acdcba0
enzo <enzo.danjour@gmail.com>
2021-06-09 08:19:31 -0300

    Add new font style

--- lib/themes/app_text_styles.dart
+++ lib/themes/app_text_styles.dart
@@ -5,16 +5,18 @@ import 'package:split_it/themes/app_theme.dart';
 abstract class AppTextStyles {
   TextStyle get title;
   TextStyle get button;
+  TextStyle get name;
 }
 
 class AppTextStylesDefaut implements AppTextStyles {
-  TextStyle get button =>  GoogleFonts.inter(
-                          color: AppTheme.colors.button,
-                          fontSize: 16,
-                          fontWeight: FontWeight.w400);
+  TextStyle get button => GoogleFonts.inter(
+      color: AppTheme.colors.button, fontSize: 16, fontWeight: FontWeight.w400);
 
   TextStyle get title => GoogleFonts.montserrat(
-                        color: AppTheme.colors.title,
-                        fontSize: 40,
-                        fontWeight: FontWeight.w700);
+      color: AppTheme.colors.title, fontSize: 40, fontWeight: FontWeight.w700);
+
+  TextStyle get name => GoogleFonts.montserrat(
+      color: AppTheme.colors.backgroundPrimary,
+      fontSize: 24,
+      fontWeight: FontWeight.w700);
 }

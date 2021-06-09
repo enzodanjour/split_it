ccdbc3e8914414215303672713c42b59bd0d8cc5
enzo <enzo.danjour@gmail.com>
2021-06-09 08:18:59 -0300

    Add custom appBar

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -10,23 +10,66 @@ class HomePage extends StatelessWidget {
     final UserModel user =
         ModalRoute.of(context)!.settings.arguments as UserModel;
     return Scaffold(
-      appBar: 
-      // AppBar(
-      //   leading: ClipRRect(
-      //     borderRadius: BorderRadius.circular(8),
-      //     child: Image.network(user.photoUrl!)
-      //   ),
-      //   title: Text(user.name!),
-      // ),
-      PreferredSize(
-        preferredSize: Size(double.infinity,244),
-        
-        child: Container(
-          decoration: BoxDecoration(
-            gradient: AppTheme.gradients.background
-          ),
-        )
-      ),
+      appBar:
+          PreferredSize(
+              preferredSize: Size.fromHeight(244),
+              child: Container(
+                decoration:
+                    BoxDecoration(gradient: AppTheme.gradients.background),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Stack(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 24
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  ClipRRect(
+                                    borderRadius: BorderRadius.circular(8),
+                                    child: Container(
+                                      color: Colors.white,
+                                      child: Image.network(user.photoUrl!),
+                                      width: 56,
+                                      height: 56,
+                                    ),
+                                  ),
+                                  SizedBox(width: 16),
+                                  Text(user.name!, style: AppTheme.textStyles.name),
+                                ],
+                              ),
+                              Container(
+                                decoration: BoxDecoration(
+                                    color: Colors.transparent,
+                                    border: Border.all(
+                                        color: Color(0xFFFFFFFF), width: 1),
+                                    borderRadius: BorderRadius.circular(8)),
+                                width: 48,
+                                height: 56,
+                                child: Center(
+                                  child: Text(
+                                    '+',
+                                    style: TextStyle(
+                                        backgroundColor: Colors.transparent,
+                                        color: Colors.white,
+                                        fontSize: 24),
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              )),
     );
   }
 }
